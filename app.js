81b3b01b6c0f71e40d584f6f619dcc386402b8a1
jameshua16 <56073320+jameshua16@users.noreply.github.com>
2020-06-13 00:25:38 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+function maze(x,y){
+    var totalCells = x*y
+    var cells = []
+    var unvisited = []
+    for(var i = 0; i < y;i++){
+        cells[i] = []
+        unvisited[i] = []
+        for(var j = 0; j < x; j++){
+            cells[i][j] = [0,0,0,0]
+            unvisited[i][j] = true
+        }
+    }
+    
+    var currentPos = [Math.floor(Math.random()*y), Math.floor(Math.random()*x)]
+    var path = [currentPos]
+    unvisited[currentPos[0]][currentPos[1]] = false
+    var visited = 1
+    while(visited<totalCells){
+        var fourCells = [
+            [currentPos[0],currentPos[1]+1,1,3],
+            [currentPos[0]+1,currentPos[1],2,0],
+            [currentPos[0],currentPos[1]-1,3,1],
+            [currentPos[0]-1,currentPos[1],0,2],
+        ]
+        
+        var potential = []
+        for(let k = 0;k < 4;k++){
+            if((fourCells[k][0] > -1 && fourCells[k][0] < y 
+                && fourCells[k][1] > -1 && fourCells[k][1] < x 
+                && unvisited[fourCells[k][0]][fourCells[k][1]])){
+                   potential.push(fourCells[k])
+               }
+        }
+        if(potential.length){
+            nextPos = potential[Math.floor(Math.random()*potential.length)]
+            cells[currentPos[0]][currentPos[1]][nextPos[2]] = 1
+            cells[nextPos[0]][nextPos[1]][nextPos[3]] = 1
+            unvisited[nextPos[0]][nextPos[1]] = false
+            currentPos = [nextPos[0], nextPos[1]]
+            visited++
+            path.push(currentPos)
+        }
+        else{
+            currentPos = path.pop()
+        }
+    }
+    cells[0][0][3]=1
+    cells[x-1][y-1][1]=1
+    return cells
+}
+//maze(10,10)
